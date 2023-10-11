1c7f72ab14110d4189ab7c4990dc5fe5d5512e80
Madhuri Bisen <madhuri.bisen@paytm.com>
2023-10-11 19:51:20 +0530

    offer existing and black out date scripts

--- src/main/java/constants/AppConstants.java
+++ src/main/java/constants/AppConstants.java
@@ -4,5 +4,8 @@ public class AppConstants {
     public static final String STAG_URL = "http://seller-staging.paytm.com/login";
     public static final String DEV_URL = "http://seller-dev.paytm.com/login";
     public static final String FILE_UPLOADED_SUCCESSFULLY = "File uploaded successfully";
-
+    public static final String UPLOAD_BLACKOUT_DATES = "Upload Blackout Dates";
+    public static final String UPLOAD_OFFER = "Upload Offers";
+    public static final String DOWNLOAD_OFFER = "Download Offers";
+    public static final String DOWNLOAD_BLACKOUT_DATES = "Download Blackout Dates";
 }
--- src/test/java/tests/OfferUploadTest.java
+++ src/test/java/tests/OfferUploadTest.java
@@ -12,8 +12,8 @@ public class OfferUploadTest extends BaseTest {
     OfferPage offerPage;
     LoginPage loginPage;
 
-    @Test
-    public void offerUploadTest() {
+    @Test(priority = 1, description = "Verify Offer Upload for Default Cohort")
+    public void offerUploadForDefaultCohortTest() {
         loginPage = new LoginPage(PageManager.getPage());
         loginPage.login(properties.getProperty("username"), properties.getProperty("password"));
         String url = loginPage.getPageURL();
@@ -24,10 +24,69 @@ public class OfferUploadTest extends BaseTest {
         offerPage.verifyManageOffer();
         offerPage.verifyAndClickOnOfferDropdown();
         offerPage.clickOnOffersUploadValue();
+        Assert.assertEquals(offerPage.verifyUploadOffersText(), AppConstants.UPLOAD_OFFER, "Upload Offers test is not visible");
         Assert.assertTrue(offerPage.verifyUploadOffersPage(), "Upload Offers is not visible");
-        offerPage.uploadFile();
+        offerPage.uploadFileForDefaultCohort();
         Assert.assertEquals(offerPage.verifyFileUploadedSuccessfully(), AppConstants.FILE_UPLOADED_SUCCESSFULLY, "File is not uploaded successfully");
         offerPage.clickOnClearButton();
     }
+
+    @Test(priority = 2, description = "Verify Offer Upload for Online Cohort")
+    public void offerUploadForOnlineCohortTest() {
+        loginPage = new LoginPage(PageManager.getPage());
+        loginPage.login(properties.getProperty("username"), properties.getProperty("password"));
+        String url = loginPage.getPageURL();
+        Assert.assertEquals(url, AppConstants.DEV_URL, "URL is not correct");
+        offerPage = new OfferPage(PageManager.getPage());
+        offerPage.verifyAndClickOnRewardsPay();
+        offerPage.verifyAndClickOnOffers();
+        offerPage.verifyManageOffer();
+        offerPage.verifyAndClickOnOfferDropdown();
+        offerPage.clickOnOffersUploadValue();
+        Assert.assertEquals(offerPage.verifyUploadOffersText(), AppConstants.UPLOAD_OFFER, "Upload Offers test is not visible");
+        Assert.assertTrue(offerPage.verifyUploadOffersPage(), "Upload Offers is not visible");
+        offerPage.uploadFileForOnlineCohort();
+        Assert.assertEquals(offerPage.verifyFileUploadedSuccessfully(), AppConstants.FILE_UPLOADED_SUCCESSFULLY, "File is not uploaded successfully");
+        offerPage.clickOnClearButton();
+    }
+
+    @Test(priority = 3, description = "Verify Offer Upload for Offline Cohort")
+    public void offerUploadForOfflineCohortTest() {
+        loginPage = new LoginPage(PageManager.getPage());
+        loginPage.login(properties.getProperty("username"), properties.getProperty("password"));
+        String url = loginPage.getPageURL();
+        Assert.assertEquals(url, AppConstants.DEV_URL, "URL is not correct");
+        offerPage = new OfferPage(PageManager.getPage());
+        offerPage.verifyAndClickOnRewardsPay();
+        offerPage.verifyAndClickOnOffers();
+        offerPage.verifyManageOffer();
+        offerPage.verifyAndClickOnOfferDropdown();
+        offerPage.clickOnOffersUploadValue();
+        Assert.assertEquals(offerPage.verifyUploadOffersText(), AppConstants.UPLOAD_OFFER, "Upload Offers test is not visible");
+        Assert.assertTrue(offerPage.verifyUploadOffersPage(), "Upload Offers is not visible");
+        offerPage.uploadFileForOfflineCohort();
+        Assert.assertEquals(offerPage.verifyFileUploadedSuccessfully(), AppConstants.FILE_UPLOADED_SUCCESSFULLY, "File is not uploaded successfully");
+        offerPage.clickOnClearButton();
+    }
+
+    @Test(priority = 4, description = "Verify blackout dates offer Upload")
+    public void offerBlackoutDatesUploadTest() {
+        loginPage = new LoginPage(PageManager.getPage());
+        loginPage.login(properties.getProperty("username"), properties.getProperty("password"));
+        String url = loginPage.getPageURL();
+        Assert.assertEquals(url, AppConstants.DEV_URL, "URL is not correct");
+        offerPage = new OfferPage(PageManager.getPage());
+        offerPage.verifyAndClickOnRewardsPay();
+        offerPage.verifyAndClickOnOffers();
+        offerPage.verifyManageOffer();
+        offerPage.verifyAndClickOnOfferDropdown();
+        offerPage.clickOnBlackoutDatesUploadValue();
+        Assert.assertEquals(offerPage.verifyUploadBlackoutDatesPage(), AppConstants.UPLOAD_BLACKOUT_DATES, "Upload Blackout Dates is not visible");
+        Assert.assertTrue(offerPage.verifyUploadOffersPage(), "Upload Offers is not visible");
+        offerPage.blackoutDatesUploadFile();
+        Assert.assertEquals(offerPage.verifyFileUploadedSuccessfully(), AppConstants.FILE_UPLOADED_SUCCESSFULLY, "File is not uploaded successfully");
+        offerPage.clickOnClearButton();
+    }
+
 }
 
